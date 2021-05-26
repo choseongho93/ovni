b6f18c4767b16ac62d44ef8e91b0a26ce328ebcf
Nate <choseongho93@naver.com>
2021-05-26 13:56:49 +0900

    dto & entity add

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesModule } from './movies/movies.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule],
   controllers: [AppController, MoviesController],
   providers: [AppService],
 })
