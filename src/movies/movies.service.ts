19a1b826121f8b35d04bf3ee6435c7aa4c5d41a4
Nate <choseongho93@naver.com>
2021-06-02 13:19:06 +0900

    service update 2

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Movie } from './entities/movie.entity'
 
+// 여기는 Model쪽이라고 생각하면 됨
 @Injectable()
 export class MoviesService {
     private movies : Movie[] = [];
 
+    getAll(): Movie[] {
+        return this.movies;
+    }
+
+    getOne(id:string):Movie{
+        return this.movies.find(movie => movie.id === parseInt(id));
+    }
 
 }
